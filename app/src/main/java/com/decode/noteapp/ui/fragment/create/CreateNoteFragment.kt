4c5c82eb975e4b69080c629304a5ccbcbf95eba4
Onur Can Öğünç <ogunc606@gmail.com>
2023-08-18 15:40:20 +0300

    Design done

--- app/src/main/java/com/decode/noteapp/ui/fragment/create/CreateNoteFragment.kt
+++ app/src/main/java/com/decode/noteapp/ui/fragment/create/CreateNoteFragment.kt
@@ -1,32 +1,29 @@
-package com.decode.noteapp.ui.fragment.home
+package com.decode.noteapp.ui.fragment.create
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.decode.noteapp.R
-import com.decode.noteapp.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeFragment : Fragment() {
-    private lateinit var binding: FragmentHomeBinding
+class CreateNoteFragment : Fragment() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentHomeBinding.inflate(inflater,container,false)
-        binding.floatingActionButton.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_createNoteFragment)
-        }
-        return binding.root
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_note, container, false)
     }
+
 
 }
